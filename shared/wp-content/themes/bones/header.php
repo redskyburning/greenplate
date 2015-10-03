e1dea4a4a9ef179348df2d02a136e4d6c216ee69
Ben Webb <bwebb@stelladot.com>
2015-10-03 16:01:02 -0700

    now with bueno and less

--- shared/wp-content/themes/bones/header.php
+++ shared/wp-content/themes/bones/header.php
@@ -14,8 +14,6 @@
 		<title><?php wp_title(''); ?></title>
 
 		<?php // mobile meta (hooray!) ?>
-		<meta name="HandheldFriendly" content="True">
-		<meta name="MobileOptimized" content="320">
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
 		<?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
@@ -42,36 +40,34 @@
 
 	<body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
 
-		<div id="container">
+		<div id="site-container">
 
-			<header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
+			<header id="site-header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
 
-				<div id="inner-header" class="wrap cf">
+				<div class="header-logo">
+					<a href="<?php echo home_url(); ?>" rel="nofollow">
+						<img src="<?php echo get_template_directory_uri(); ?>/library/images/logo-with-text.svg" />
+					</a>
+					<div class="site-name">
+						<?php bloginfo('name'); ?>
+					</div>
+				</div>
 
-					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-					<p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+				<div class="header-nav">
 
-					<?php // if you'd like to use the site description you can un-comment it below ?>
-					<?php // bloginfo('description'); ?>
-
-
-					<nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
-						<?php wp_nav_menu(array(
-    					         'container' => false,                           // remove nav container
-    					         'container_class' => 'menu cf',                 // class of container (should you choose to use it)
-    					         'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
-    					         'menu_class' => 'nav top-nav cf',               // adding custom nav class
-    					         'theme_location' => 'main-nav',                 // where it's located in the theme
-    					         'before' => '',                                 // before the menu
-        			               'after' => '',                                  // after the menu
-        			               'link_before' => '',                            // before each link
-        			               'link_after' => '',                             // after each link
-        			               'depth' => 0,                                   // limit the depth of the nav
-    					         'fallback_cb' => ''                             // fallback function (if there is one)
-						)); ?>
-
-					</nav>
-
+					<?php wp_nav_menu(array(
+						'container' => false,                           // remove nav container
+						'container_class' => '',                 // class of container (should you choose to use it)
+						'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
+						'menu_class' => 'header-nav-menu',               // adding custom nav class
+						'theme_location' => 'main-nav',                 // where it's located in the theme
+						'before' => '',                                 // before the menu
+						'after' => '',                                  // after the menu
+						'link_before' => '',                            // before each link
+						'link_after' => '',                             // after each link
+						'depth' => 0,                                   // limit the depth of the nav
+						'fallback_cb' => ''                             // fallback function (if there is one)
+					)); ?>
 				</div>
 
 			</header>
---
+++ shared/wp-content/themes/bueno/gulpfile.js
@@ -0,0 +1,36 @@
+var gulp        = require('gulp');
+var less        = require('gulp-less');
+var map         = require('map-stream');
+var rename      = require('gulp-rename');
+var concat      = require('gulp-concat');
+var browserSync = require('browser-sync').create();
+var postcss     = require('gulp-postcss');
+
+var serverUrl   = 'greenplatespecial.dev';
+var lessGlob    = 'less/**/*.less';
+var templGlob   = '**/*.php';
+
+
+gulp.task('default', function() {
+    gulp.src('less/root.less')
+        .pipe(less())
+        .pipe( postcss([ require('autoprefixer')]) )
+        .pipe(rename('root.css'))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch',function(){
+    gulp.watch(lessGlob,['default']);
+});
+
+gulp.task('reload', function() {
+    browserSync.reload();
+});
+
+gulp.task('server',['vendor-css','default'],function(){
+    browserSync.init({
+        proxy: serverUrl
+    });
+    gulp.watch(lessGlob,['default','reload']);
+    gulp.watch(templGlob,['reload']);
+});

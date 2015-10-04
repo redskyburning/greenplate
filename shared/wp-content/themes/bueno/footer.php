dc4e3997da664e81d5bbfe385d19aed260290ad1
Ben Webb <bwebb@stelladot.com>
2015-10-04 12:27:43 -0700

    css fix

--- shared/wp-content/themes/bueno/footer.php
+++ shared/wp-content/themes/bueno/footer.php
@@ -2,7 +2,10 @@
 	<div id="knockout-slogan">
 		<img src="<?php bloginfo('template_directory'); ?>/images/slogan-white.svg" />
 	</div>
-	
+
+	<div id="footer-contact">
+	 &copy; <?php echo date("Y"); ?> Green Plate Special | <a href="tel:2063107393">206-310-7393</a> | 2115 25th Ave S, Seattle, WA 98144
+	</div>
 </div><!-- /#container -->
 <?php wp_footer(); ?>
 

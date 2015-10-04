1f303537f21603e27de5e3d871a327d852897951
Ben Webb <bwebb@stelladot.com>
2015-10-04 13:55:37 -0700

    gallery template fix

--- shared/wp-content/themes/bueno/template-homepage.php
+++ shared/wp-content/themes/bueno/template-homepage.php
@@ -14,53 +14,36 @@ get_header();
 				<img src="<?php bloginfo('template_directory'); ?>/images/text-horizontal-white.svg" />
 			</div>
 
-			<?php
-				$images = array(
-					'IMAG1099.jpg',
-					'photo1-cassandra.jpg',
-					'IMAG1211.jpg',
-				);
 
-				$images = array_diff(scandir(getcwd() . '/wp-content/themes/bueno/gallery_images'), array('..', '.'));
-				sort($images);
-			?>
+			<?php if( have_rows('image_carousel') ): ?>
 
-			<div id="homepage-gallery" class="carousel slide" data-ride="carousel">
-				<!-- Indicators -->
-				<ol class="carousel-indicators">
+				<div id="homepage-gallery" class="carousel slide" data-ride="carousel">
+					<!-- Indicators -->
+					<ol class="carousel-indicators">
 
-					<?php foreach($images as $i => $image): ?>
+						<?php $i = 0; while ( have_rows('program') ) : the_row(); ?>
 
-						<li data-target="#carousel-example-generic" data-slide-to="<?php echo $i ?>" class="<?php echo $i == 0 ? 'active' : ''; ?>"></li>
+							<li data-target="#carousel-example-generic" data-slide-to="<?php echo $i ?>" class="<?php echo $i == 0 ? 'active' : ''; ?>"></li>
 
-					<?php endforeach; ?>
+						<?php $i++; endwhile; ?>
 
-				</ol>
+					</ol>
 
-				<!-- Wrapper for slides -->
-				<div class="carousel-inner" role="listbox">
+					<!-- Wrapper for slides -->
+					<div class="carousel-inner" role="listbox">
 
-					<?php foreach($images as $i => $image): ?>
+						<?php $i = 0; while ( have_rows('program') ) : the_row(); ?>
 
-						<div class="item <?php echo $i == 0 ? 'active' : ''; ?>">
-							<!--<div class="gallery-image" style="background-image:url('<?php /*bloginfo('template_directory'); echo "/gallery_images/$image"; */?>');">&nbsp;</div>-->
-							<img class="gallery-image" src="<?php bloginfo('template_directory'); echo "/gallery_images/$image"; ?>">
-						</div>
+							<div class="item <?php echo $i == 0 ? 'active' : ''; ?>">
+								<img class="gallery-image" src="<?php the_sub_field('image') ?>"">
+							</div>
 
-					<?php endforeach; ?>
+						<?php $i++; endwhile; ?>
 
+					</div>
 				</div>
 
-				<!-- Controls -->
-				<!--<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-					<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-					<span class="sr-only">Previous</span>
-				</a>
-				<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-					<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-					<span class="sr-only">Next</span>
-				</a>-->
-			</div>
+			<?php endif; ?>
 
             <?php if (have_posts()) : $count = 0; ?>
             <?php while (have_posts()) : the_post(); $count++; ?>

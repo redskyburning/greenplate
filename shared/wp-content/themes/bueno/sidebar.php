dd2ee20f3fc3523f6cbd1a30b3f7c696949bc666
Ben Webb <bwebb@stelladot.com>
2015-10-04 11:50:50 -0700

    sidebar link urls

--- shared/wp-content/themes/bueno/sidebar.php
+++ shared/wp-content/themes/bueno/sidebar.php
@@ -10,12 +10,12 @@
 			<img src="<?php bloginfo('template_directory'); ?>/images/diggit.svg" />
 		</div>
 		<p>Help us inspire and empower youth to experience food in new ways through gardening, cooking, and eating together.</p>
-		<a class="sidebar-button" href="#">Donate!</a   >
+		<a class="sidebar-button" href="https://co.clickandpledge.com/?wid=75220">Donate!</a   >
 	</div>
 
 	<div class="sidebar-donate">
 		<p>Sign up for our mailing list to receive updates about programs, events, and volunteer opportunities.</p>
-		<a class="sidebar-button" href="#">subscribe!</a   >
+		<a class="sidebar-button" href="http://visitor.r20.constantcontact.com/d.jsp?llr=ljhiirnab&p=oi&m=1114049355051&sit=bh94t95hb&f=87f050a3-1bfb-4914-a10d-176a231b0267">subscribe!</a   >
 	</div>
 
 	<?php woo_sidebar(1); ?>		           
